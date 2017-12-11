3c913a63a57f79feef0b0782438e96e5a38b74c3
git <git>
2017-12-11 13:19:34 +0800

    update

--- example-release/redis-service-release/deploy.sh
+++
@@ -1,1 +0,0 @@
-bosh2 -e vbox -d redis-deployment deploy ./manifests/bosh_lite_manifest.yml<<<y
---
+++ example-release/redis-service-release/deploy_small.sh
@@ -0,0 +1,1 @@
+bosh2 -e vbox -d redis-deployment deploy ./manifests/bosh_lite_manifest_small.yml<<<y
